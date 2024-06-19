f6605478105830271fed0b730c9daa2b93cbd155
TutaE123 <tutaeva.ekaterina@list.ru>
2024-06-19 11:11:36 +0500

    Изменение 2

--- app/views.py
+++ app/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.utils import timezone
 from .models import Order
 from django.contrib.auth.decorators import login_required
 from .forms import OrderForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 
 
